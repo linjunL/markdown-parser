dc50c917e77867f787d61e7ab3397b1d07e113f6
linjunL <lil023@ucsd.edu>
2022-04-21 18:20:45 -0700

    Fix the bug for last line with non-link thing
    Rukias-Mac-Pro:markdown-parser liulinyun$ javac MarkdownParse.java
    Rukias-Mac-Pro:markdown-parser liulinyun$ java MarkdownParse test-file2.md
    Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
            at java.base/java.lang.StringLatin1.newString(StringLatin1.java:769)
            at java.base/java.lang.String.substring(String.java:2709)
            at MarkdownParse.getLinks(MarkdownParse.java:21)
            at MarkdownParse.main(MarkdownParse.java:32)

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,10 +13,8 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
-            if (openBracket > 0 && markdown.charAt(openBracket - 1 ) == '!'){
-                openBracket = markdown.indexOf("[",openBracket + 1);
-            }
-            if (openBracket < 0){
+            // fix the test-file 2
+            if (openBracket == -1){
                 return toReturn;
             }
             
