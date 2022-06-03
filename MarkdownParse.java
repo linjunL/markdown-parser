08e7f65e01e1730e2e2d12a3ac7971854f45ce68
linjunL <lil023@ucsd.edu>
2022-06-03 14:24:27 -0700

    Chaneg

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,4 +1,3 @@
-//https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -6,39 +5,37 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParse {
-
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then read link upto next )
+        // find the next [, then find the ], then find the (, then take up to
+        // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            if (openBracket > 0 && markdown.charAt(openBracket - 1) == '!'){
-                openBracket = markdown.indexOf("[" , openBracket + 1);
-            }
-            // fix the test-file 2
-            if (openBracket == -1){
-                return toReturn;
-            }
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
+            int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            // System.out.format("%d\t%d\t%s\n", currentIndex, nextOpenBracket, toReturn);
+            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+            int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if (openParen == -1 || closeParen == -1){
-                openParen =  markdown.indexOf("[", closeBracket);
-                closeParen = markdown.indexOf("]",openParen);
+            if(nextOpenBracket == -1 || nextCloseBracket == -1 ||
+                     openParen == -1 || closeParen == -1) {
+                break;
             }
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if(nextOpenBracket != 0 && markdown.charAt(nextOpenBracket - 1) == '!') {
+                currentIndex = currentIndex + 1;
+                continue;
+            }
+            String link = markdown.substring(openParen + 1, closeParen);
+            if(link.indexOf(" ") == -1) {
+                toReturn.add(link);
+            }
             currentIndex = closeParen + 1;
         }
-
         return toReturn;
     }
-
-
     public static void main(String[] args) throws IOException {
-        Path fileName = Path.of(args[0]);
-        String content = Files.readString(fileName);
-        ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+		Path fileName = Path.of(args[0]);
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = getLinks(contents);
+        System.out.println(links);
     }
 }
---
+++ MarkdownParseTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class MarkdownParseTest {
+
+
+    @Test
+    public void testSpaceAfterParen() {
+        String contents = "[title]( space-in-url.com)";
+        List<String> expect = List.of("space-in-url.com");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    
+}
